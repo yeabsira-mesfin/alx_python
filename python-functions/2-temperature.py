3fc2668c6e6a9dcd63d0b1fa7884d244bce155fb
yeabsira_mesfin <yabidesta@gmail.com>
2023-07-31 13:43:11 -0400

    C to F

---
+++ python-functions/2-temperature.py
@@ -0,0 +1,6 @@
+def convert_to_celsius(fahrenheit):
+    return (fahrenheit * 1.8) + 32
+print(convert_to_celsius(100))
+print(convert_to_celsius(-40))
+print(convert_to_celsius(-459.67))
+print(convert_to_celsius(32))
