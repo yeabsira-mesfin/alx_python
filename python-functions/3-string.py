feeca5b31dfe19d0148c505296361d07fba1d6c2
yeabsira_mesfin <yabidesta@gmail.com>
2023-07-31 13:49:03 -0400

    Reverse

---
+++ python-functions/3-string.py
@@ -0,0 +1,6 @@
+def reverse_string(string):
+    return string[::-1]
+print(reverse_string("Hello"))
+print(reverse_string(""))
+print(reverse_string("madam"))
+print(reverse_string("Hello, World!"))
