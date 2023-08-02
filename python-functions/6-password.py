d8fc0f17c2f4a3eebb36e4912ba1a24710cbb65d
yeabsira_mesfin <yabidesta@gmail.com>
2023-08-02 11:20:18 -0400

    Test

--- python-functions/6-password.py
+++ python-functions/6-password.py
@@ -21,7 +21,3 @@ def validate_password(password):
         return False
 
     return True
-print(validate_password("Password123"))
-print(validate_password("abc123"))
-print(validate_password("Password 123"))
-print(validate_password("password123"))
