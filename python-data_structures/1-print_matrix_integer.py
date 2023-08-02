9b3694467e0628b8c0630a5f11e4fdde4fd6ce0b
yeabsira_mesfin <yabidesta@gmail.com>
2023-08-02 16:55:08 -0400

    print matrix

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,16 +1,5 @@
 def print_matrix_integer(matrix=[[]]):
-    if not matrix:
-        print("Matrix is empty")
-        return
-
-    rows = len(matrix)
-    cols = len(matrix[0])
-
-   
-    column_widths = [max(len(str(matrix[i][j])) for i in range(rows)) for j in range(cols)]
-
-    for i in range(rows):
-        for j in range(cols):
-           
-            print("{:>{width}}".format(matrix[i][j], width=column_widths[j]), end=" ")
+    for row in matrix:
+        for element in row:
+            print("{:d}".format(element), end=" ")
         print()
