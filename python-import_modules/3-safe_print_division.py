0cb0063a2583dcfb7accee146b7d10a46f90c8d5
yeabsira_mesfin <yabidesta@gmail.com>
2023-08-02 14:39:31 -0400

    safe print division

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -8,7 +8,9 @@ def safe_print_division(a, b):
         print(f"An unexpected error occurred: {e}")
         return None
     else:
-        print("Inside result: {}".format(result))
+        print("{:d} / {:d} = {}".format(a, b, result))
         return result
     finally:
         print("Finally block executed.")
+        
+print(safe_print_division(10,b))
