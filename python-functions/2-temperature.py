e8982c559f293387b3e82fce04af8380d3fe8c33
yeabsira_mesfin <yabidesta@gmail.com>
2023-08-02 11:14:19 -0400

    Test

--- python-functions/2-temperature.py
+++ python-functions/2-temperature.py
@@ -1,2 +1,2 @@
 def convert_to_celsius(fahrenheit):
-    return (fahrenheit * 1.8) + 32
+    return fahrenheit * 1.8 + 32
--- python-functions/3-string.py
+++ python-functions/3-string.py
@@ -1,6 +1,2 @@
 def reverse_string(string):
     return string[::-1]
-print(reverse_string("Hello"))
-print(reverse_string(""))
-print(reverse_string("madam"))
-print(reverse_string("Hello, World!"))
