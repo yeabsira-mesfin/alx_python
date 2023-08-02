ea9cc70d6d04db41b86df12326a11542c6b4c43b
yeabsira_mesfin <yabidesta@gmail.com>
2023-08-02 17:16:14 -0400

    square matrix

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,8 @@
+def square_matrix_simple(matrix=[]):
+    result_matrix = [[0 for _ in range(len(matrix[0]))] for _ in range(len(matrix))]
+
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            result_matrix[i][j] = matrix[i][j] ** 2
+
+    return result_matrix
