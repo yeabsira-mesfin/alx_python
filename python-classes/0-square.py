10f7de4db2016a6f6ae4d7fec5d5ad89db35c898
yeabsira_mesfin <yabidesta@gmail.com>
2023-08-08 14:25:53 -0400

    square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,12 @@
 class Square:
-    def __init__(self,size):
-        self.size = size
+    def __init__(self, size):
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
+
+    def perimeter(self):
+        return 4 * self.__size
+
+    def __str__(self):
+        return f"Square with side length {self.__size}"
