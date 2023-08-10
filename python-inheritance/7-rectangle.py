83714f4bdc7fb7d2a11f10bf9a06bdba97869ef8
yeabsira_mesfin <yabidesta@gmail.com>
2023-08-10 11:58:36 +0300

    rectangel

---
+++ python-inheritance/7-rectangle.py
@@ -0,0 +1,24 @@
+class BaseGeometry:
+    """Empty class base geometry"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+class Rectangle(BaseGeometry):
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", self.__width)
+        self.integer_validator("height", self.__height)
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return f"[Rectangle] {self.__width}/{self.__height}"
